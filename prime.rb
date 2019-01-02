17e7d88abb021194108d18006716f5b80df3e21c
Trent Moss <trentmoss05@gmail.com>
2019-01-02 05:27:47 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,8 +1,10 @@
 # Add  code here!
 def prime?(number)
- if number % 2 == 0
-   true
+ if number < 0 || number == 0 || number == 1
+   return false
  else
-   false
+   (2..number-1).to_a.all? do |possible_factor|
+     number % possible_factor != 0
+   end
  end
 end
